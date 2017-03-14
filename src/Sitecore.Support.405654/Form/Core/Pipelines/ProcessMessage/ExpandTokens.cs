18d8d2f4b7f2cc1cca23382058de6779318100fc
Pavel <piv@sitecore.net>
2017-03-14 13:57:43 +0200

    Fix issue with multiple line field

--- src/Sitecore.Support.405654/Form/Core/Pipelines/ProcessMessage/ExpandTokens.cs
+++ src/Sitecore.Support.405654/Form/Core/Pipelines/ProcessMessage/ExpandTokens.cs
@@ -1,37 +1,42 @@
-﻿namespace Sitecore.Support.Form.Core.Pipelines.ProcessMessage
-{
-    using Sitecore.Form.Core.Configuration;
-    using Sitecore.Form.Core.Controls.Data;
-    using Sitecore.Form.Core.Pipelines.ProcessMessage;
-    using Sitecore.Forms.Core.Data;
-    using Sitecore.StringExtensions;
+﻿using System;
 
-    public class ExpandTokens
+namespace Sitecore.Support.Form.Core.Pipelines.ProcessMessage
+{
+  using Sitecore.Form.Core.Configuration;
+  using Sitecore.Form.Core.Controls.Data;
+  using Sitecore.Form.Core.Pipelines.ProcessMessage;
+  using Forms.Core.Data;
+  using StringExtensions;
+
+  public class ExpandTokens
+  {
+    #region New code
+    public void Process(ProcessMessageArgs args)
     {
-        #region New code
-        public void Process(ProcessMessageArgs args)
+      foreach (AdaptedControlResult result in args.Fields)
+      {
+        var item = new FieldItem(StaticSettings.ContextDatabase.GetItem(result.FieldID));
+        var str = result.Value;
+
+        if (args.MessageType == MessageType.SMS) continue;
+        var str2 = args.Mail.ToString();
+        var index = str2.IndexOf("[<label id=\"{0}\">".FormatWith(item.ID), StringComparison.Ordinal);
+
+        if (index <= -1) continue;
+        var num2 = str2.IndexOf("</label>]", index, StringComparison.Ordinal);
+
+        if (num2 <= -1) continue;
+        num2 += "</label>]".Length;
+        var oldValue = str2.Substring(index, num2 - index);
+
+        if (!string.IsNullOrEmpty(result.Parameters) && result.Parameters.StartsWith("multipleline") && args.IsBodyHtml)
         {
-            foreach (AdaptedControlResult result in args.Fields)
-            {
-                FieldItem item = new FieldItem(StaticSettings.ContextDatabase.GetItem(result.FieldID));
-                string str = result.Value;
-                if (args.MessageType != MessageType.SMS)
-                {
-                    string str2 = args.Mail.ToString();
-                    int index = str2.IndexOf("[<label id=\"{0}\">".FormatWith(new object[] { item.ID }));
-                    if (index > -1)
-                    {
-                        int num2 = str2.IndexOf("</label>]", index);
-                        if (num2 > -1)
-                        {
-                            num2 += "</label>]".Length;
-                            string oldValue = str2.Substring(index, num2 - index);
-                            args.Mail.Replace(oldValue, str ?? string.Empty);
-                        }
-                    }
-                }
-            }
-        } 
-        #endregion
+          str = str.Replace(Environment.NewLine, "<br/>");
+        }
+
+        args.Mail.Replace(oldValue, str ?? string.Empty);
+      }
     }
+    #endregion
+  }
 }
